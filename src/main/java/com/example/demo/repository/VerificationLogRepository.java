a22b3e7745fa9a88363feec513664c8c2b263e9b
bala-amypo <bala@amypo.in>
2025-12-26 18:58:17 +0000

    User commit

--- src/main/java/com/example/demo/repository/VerificationLogRepository.java
+++ src/main/java/com/example/demo/repository/VerificationLogRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.model.VerificationLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;   // ✅ MISSING IMPORT FIXED
+import java.util.List;   
 
 public interface VerificationLogRepository extends JpaRepository<VerificationLog, Long> {
 
